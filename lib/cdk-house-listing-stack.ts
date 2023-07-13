3c32fbf7f0847fae333a269822163a71c25eedd4
CinArb2 <cindypao27@gmail.com>
2023-07-13 07:10:31 -0500

    create cdk stack for listings route

--- lib/cdk-house-listing-stack.ts
+++ lib/cdk-house-listing-stack.ts
@@ -1,19 +1,61 @@
-import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Duration, RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { AttributeType, BillingMode, Table } from 'aws-cdk-lib/aws-dynamodb';
+import { Runtime } from 'aws-cdk-lib/aws-lambda';
+import { NodejsFunction, NodejsFunctionProps } from 'aws-cdk-lib/aws-lambda-nodejs';
+import { join } from 'path'
+import { IResource, LambdaIntegration, MockIntegration, PassthroughBehavior, RestApi } from 'aws-cdk-lib/aws-apigateway';
+import { RetentionDays } from 'aws-cdk-lib/aws-logs';
+
 
 export class CdkHouseListingStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'CdkHouseListingQueue', {
-      visibilityTimeout: Duration.seconds(300)
+    // create Dynamodb table
+    const dynamoTable = new Table(this, 'house-listing-CDK', {
+      partitionKey: {
+        name: 'id',
+        type: AttributeType.STRING
+      },
+      tableName: 'house-listing',
+      billingMode: BillingMode.PAY_PER_REQUEST,
+      removalPolicy: RemovalPolicy.DESTROY,
     });
 
-    const topic = new sns.Topic(this, 'CdkHouseListingTopic');
+    // Create Lambda function
+    const nodeJsFunctionProps: NodejsFunctionProps = {
+      bundling: {
+        externalModules: [
+          'aws-sdk', // Use the 'aws-sdk' available in the Lambda runtime
+        ],
+      },
+      depsLockFilePath: join(__dirname, '/../lambdas', 'package-lock.json'),
+      environment: {
+        PRIMARY_KEY: 'id',
+        TABLE_NAME: dynamoTable.tableName,
+      },
+      runtime: Runtime.NODEJS_16_X,
+      logRetention: RetentionDays.ONE_WEEK
+    }
+    // Create a Lambda function for each of the CRUD operations
+    const getAllLambda = new NodejsFunction(this, 'getAllItemsFunction', {
+      entry: join(__dirname, '/../lambdas/get-all.ts'),
+      ...nodeJsFunctionProps,
+    });
 
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    // Grant the Lambda function read access to the DynamoDB table
+    dynamoTable.grantReadWriteData(getAllLambda);
+
+    // Integrate the Lambda functions with the API Gateway resource
+    const getAllIntegration = new LambdaIntegration(getAllLambda);
+
+    // Create an API Gateway resource for each of the CRUD operations
+    const api = new RestApi(this, 'itemsApi', {
+      restApiName: 'Items Service'
+    });
+
+    const listings = api.root.addResource('listings');
+    listings.addMethod('GET', getAllIntegration);
   }
 }
